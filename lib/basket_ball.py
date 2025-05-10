69fce208ace2a5c8d8e5d6411677677c4391a696
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-05-10 22:23:21 +0300

    basket_ball

--- lib/basket_ball.py
+++ lib/basket_ball.py
@@ -182,3 +182,82 @@ def game_dict():
             ]
         }
     }
+
+def get_all_players():
+    game = game_dict()
+    return game['home']['players'] + game['away']['players']
+
+def num_points_per_game(name):
+    for player in get_all_players():
+        if player['name'] == name:
+            return player['points_per_game']
+
+def player_age(name):
+    for player in get_all_players():
+        if player['name'] == name:
+            return player['age']
+
+def team_colors(team_name):
+    game = game_dict()
+    for team in game.values():
+        if team['team_name'] == team_name:
+            return team['colors']
+        
+def team_names():
+    game = game_dict()
+    return [game['home']['team_name'], game['away']['team_name']]
+
+def player_numbers(team_name):
+    game = game_dict()
+    for team in game.values():
+        if team['team_name'] == team_name:
+            return [player['number'] for player in team['players']]
+
+def player_stats(name):
+    game = game_dict()
+    for team in game.values():
+        for player in team['players']:
+            if player['name'] == name:
+                return player
+
+def average_rebounds_by_shoe_brand():
+    game = game_dict()
+    rebounds_by_brand = {}
+
+    for team in game.values():
+        for player in team['players']:
+            rebounds = player['rebounds_per_game']
+            shoe_brand = player['shoe_brand']
+
+            if shoe_brand in rebounds_by_brand:
+                rebounds_by_brand[shoe_brand].append(rebounds)
+            else:
+                rebounds_by_brand[shoe_brand] = [rebounds]
+
+    for brand, rebounds_list in rebounds_by_brand.items():
+        average_rebounds = sum(rebounds_list) / len(rebounds_list)
+        print(f'{brand}:  {average_rebounds:.2f}')
+
+def most_career_points():
+    game = game_dict()
+    top_scorer = ""
+    max_points = 0
+
+    for team in game_values():
+        for player in team['players']:
+            if player['career_points'] > max_points:
+                max_points = player['career_points']
+                top_scorer = player['name']
+
+    return top_scorer
+
+def longest_name():
+    game = game_dict()
+    longest_name = ""
+
+    for team in game_values():
+        for player_name in team['players']:
+            if len(player_name) > len(longest_name):
+                longest_name = player_name
+        
+    print(longest_name)    
